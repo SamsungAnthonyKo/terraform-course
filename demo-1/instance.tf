10c10144e412ad6e588fe9fc116c5e38617245dd
Anthony Ko <ansik.ko@gmail.com>
2021-02-15 15:46:01 +0900

    Update instance.tf

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-northeast-2"
+}
+
 resource "aws_instance" "koas-example" {
   ami           = "ami-09282971cf2faa4c9"
   instance_type = "t2.micro"
